ca0cbca5beb3d76fce508f16bc0ffb1ee0e975a7
Sridevi Kuchibotla <sridevikuchibotla@Sridevis-MacBook-Pro.local>
2024-11-24 22:10:51 -0800

    Created <OutfitItem> component

---
+++ frontend/src/components/outfit_item/OutfitItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="outfit-item-container">
+    <OutfitItemSelect selectType="type" v-model="typeRef" />
+    <OutfitItemSelect selectType="color" v-model="colorRef" />
+    <OutfitItemUpload />
+  </div>
+</template>
+
+<script>
+  import { ref } from 'vue';
+  import OutfitItemSelect from './OutfitItemSelect.vue';
+  import OutfitItemUpload from './OutfitItemUpload.vue';
+
+  export default {
+    name: 'OutfitItem',
+    components: {
+      OutfitItemSelect,
+      OutfitItemUpload,
+    },
+    setup() {
+      const typeRef = ref(null);
+      const colorRef = ref(null);
+
+      return { typeRef, colorRef };
+    },
+  }
+</script>
+
+<style>
+  #outfit-item-container {
+    width: 25%;
+    min-width: 250px;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    border: 1px solid grey;
+    padding: 20px 20px;
+    text-align: center;
+  }
+</style>
