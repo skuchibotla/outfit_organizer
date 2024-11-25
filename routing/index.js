91af5b547ad242580d7f16c8e24ef91dcdb8e4c3
Sridevi Kuchibotla <sridevikuchibotla@Sridevis-MacBook-Pro.local>
2024-11-24 22:04:10 -0800

    Add CORS to Express server and pass in user_id as param

--- routing/index.js
+++ routing/index.js
@@ -1,21 +1,32 @@
 import express from 'express';
+import cors from 'cors';
 import axios from 'axios';
 
 const app = express();
 const port = 3000;
 
-app.get('/upload', async (req, res) => {
+// Configure CORS
+app.use(cors({
+  origin: ['http://localhost:8080', 'http://127.0.0.1:8080'],
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+}));
+
+app.use(express.json());
+
+app.post('/upload/:user_id', async (req, res) => {
+  const userId = req.params.user_id;
+
   try {
-    const response = await axios.get('http://localhost:8000/remove-background', {
-      params: {
-        image_path: req.params.image_path,
-        user_id: req.params.user_id
-      }
+    const response = await axios.post(`http://localhost:8000/remove-background/${userId}`, req, {
+      headers: {
+        'Content-Type': req.headers['content-type']
+      },
     });
 
     res.send(response.data.message);
   } catch (err) {
-    res.status(err.response.status).send(err.response.data.error);
+    res.status(err.response?.status || 500).send(err.response?.data?.error);
   }
 });
 
