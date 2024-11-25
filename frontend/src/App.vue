2fb76896312dac51e347bd57d15f823c6da846ed
Sridevi Kuchibotla <sridevikuchibotla@Sridevis-MacBook-Pro.local>
2024-11-24 22:11:04 -0800

    Render <OutfitItem> instead of <HelloWorld> default

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <OutfitItem />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+  import OutfitItem from './components/outfit_item/OutfitItem.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      OutfitItem
+    },
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+
+  #app {
+    margin-top: 60px;
+  }
 </style>
